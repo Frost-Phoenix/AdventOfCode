7edcad9cf9922cd66d96c15028579daac4293cec
Frost-Phoenix <67cyril6767@gmail.com>
2023-12-06 11:50:34 +0100

    day 6 part 2

--- day6/sol.py
+++ day6/sol.py
@@ -1,13 +1,7 @@
 # https://adventofcode.com/2023/day/6
 
 
-def part_1(file: str):
-    
-    lines = open(file, 'r').readlines()
-
-    times     = [int(x) for x in lines[0][11:-1].split(" ") if len(x) > 0]
-    distances = [int(x) for x in lines[1][11:-1].split(" ") if len(x) > 0]
-
+def solve(times: list[int], distances: list[int]) -> int:
     res = 1
 
     for i in range(len(times)):
@@ -29,10 +23,34 @@ def part_1(file: str):
 
     return res
 
+def part_1(file: str):
+    lines = open(file, 'r').readlines()
+
+    times     = [int(x) for x in lines[0][11:-1].split(" ") if len(x) > 0]
+    distances = [int(x) for x in lines[1][11:-1].split(" ") if len(x) > 0]
+
+    return solve(times, distances)
+
+def part_2(file: str):
+    lines = open(file, 'r').readlines()
+
+    times     = [x for x in lines[0][11:-1].split(" ") if len(x) > 0]
+    distances = [x for x in lines[1][11:-1].split(" ") if len(x) > 0]
+
+    time = int("".join(times))
+    distance = int("".join(distances))
+
+    return solve([time], [distance])
+
 
 def main():
+    # 160816
     part_1_sol = part_1("input.txt")
     print(f"sol part 1: {part_1_sol}")
+
+    # 46561107
+    part_2_sol = part_2("input.txt")
+    print(f"sol part 2: {part_2_sol}")
 
 
 if __name__ == "__main__":
