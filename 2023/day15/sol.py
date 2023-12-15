07ad0dfee70485a7e79a30c073e27b5d7141d44f
Frost-Phoenix <67cyril6767@gmail.com>
2023-12-15 09:58:13 +0100

    day 15 improved

--- 2023/day15/sol.py
+++ 2023/day15/sol.py
@@ -1,4 +1,4 @@
-# https://adventofcode.com/2023/day/ 
+# https://adventofcode.com/2023/day/15
 
 
 def get_hash(seq: str) -> int:
@@ -14,46 +14,32 @@ def part_1(file: str) -> int:
     return sum([get_hash(l) for l in open(file, 'r').readline().split(",")])
 
 def part_2(file: str) -> int:
-    box = {i: [None] * 10 for i in range(256)}
+    label_in_box = lambda label, box: any(filter(lambda x: False if x == None else label in x, box))
+    box = [[] for i in range(256)]
 
     for l in open(file, 'r').readline().split(','):
-        if l[-1] == '-':
-            label = l[:-1]
-            id = get_hash(label)
-            if any(filter(lambda x: False if x == None else label in x, box[id])):
-                tmp = []
-                for lens in box[id]:
-                    if lens != None and label not in lens:
-                        tmp.append(lens)
-                box[id] = tmp + [None] * (10 - len(tmp))
-        else: 
-            label = l[:-2]
-            id = get_hash(label)
-            if any(filter(lambda x: False if x == None else label in x, box[id])):
-                for i in range(len(box[id])):
-                    if label in box[id][i]:
-                        box[id][i] = l.replace("=", " ")
-                        break
-            else:
-                for i in range(len(box[id])):
-                    if box[id][i] == None:
-                        box[id][i] = l.replace("=", " ")
-                        break
+        label = l[:-1] if "-" in l else l[:-2]
+        id = get_hash(label)
+        is_in = label_in_box(label,box[id])
 
-    res = 0
-    for i,b in box.items():
-        for j,item in enumerate(b):
-            if item != None:
-                res += (i+1) * (j+1) * int(item[-1])
+        if l[-1] == '-': 
+            if is_in: box[id] = list(filter(lambda x: label not in x, box[id]))
+        elif not is_in: 
+            box[id].append(l.replace("=", " "))
+        else:
+            for i in range(len(box[id])):
+                if label in box[id][i]:
+                    box[id][i] = l.replace("=", " ")
+                    break
 
-    return res
+    return sum([sum([(i+1) * (j+1) * int(item[-1]) for j,item in enumerate(b)]) for i,b in enumerate(box)])
 
 def main() -> None:
     # 521341
     part_1_sol = part_1("input.txt")
     print(f"sol part 1: {part_1_sol}")
     
-    # 
+    # 252782
     part_2_sol = part_2("input.txt")
     print(f"sol part 2: {part_2_sol}")
 
