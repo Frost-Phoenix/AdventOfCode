6f2a4e08ce8824c3802ab497a13b01754a90346a
Frost-Phoenix <67cyril6767@gmail.com>
2023-12-15 14:49:07 +0100

    day 3 part 2

--- 2022/day03/sol.py
+++ 2022/day03/sol.py
@@ -1,20 +1,26 @@
 # https://adventofcode.com/2022/day/
 
 
+def get_char_value(char: str) -> int:
+    return ord(char) - ord("a") + 1 if char.islower() else ord(char) - ord('A') + 1 + 26
+
+def get_repeat_char(l: str) -> str:
+    for c in l[:len(l)//2]:
+        if c in l[len(l)//2:]: return c
+
 def part_1(file: str) -> int:
+    return sum([get_char_value(get_repeat_char(l[:-1])) for l in open(file, 'r').readlines()])
 
-    def get_repeat_char(l: str) -> str:
-        for c in l[:len(l)//2]:
-            if c in l[len(l)//2:]: return c
-
+def part_2(file: str) -> int:
     lines = open(file, 'r').readlines()
 
     res = 0
 
-    for l in lines:
-        l = l[:-1]
-        c = get_repeat_char(l)
-        res += ord(c) - ord("a") + 1 if c.islower() else ord(c) - ord('A') + 1 + 26
+    for i in range(0, len(lines), 3):
+        for c in lines[i][:-1]:
+            if c in lines[i+1] and c in lines[i+2]:
+                res += get_char_value(c)
+                break
 
     return res
 
@@ -22,6 +28,10 @@ def main() -> None:
     # 7581
     part_1_sol = part_1("input.txt")
     print(f"sol part 1: {part_1_sol}")
+
+    # 2525
+    part_2_sol = part_2("input.txt")
+    print(f"sol part 2: {part_2_sol}")
 
 
 if __name__  == "__main__":
